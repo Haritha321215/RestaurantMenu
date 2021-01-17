5d0f69f1200041e085d842d9d2b0151b15e8af71
Haritha321215 <nithyaprerana@gmail.com>
2021-01-16 19:20:43 -0600

    designed the Menu and menu item classes

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,17 @@
 package restaurant;
 
 public class Menu {
+    public static void main(String[] args) {
+        MenuItem biryani = new MenuItem();
+
+        biryani.setPrice(30.00);
+        System.out.println("Price:"+biryani.getPrice());
+
+        biryani.setDescription("Prepared with chicken, rice and spices.");
+        System.out.println("Description:"+biryani.getDescription());
+
+        biryani.setCategory("Main course");
+        System.out.println("Category:"+biryani.getCategory());
+
+    }
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,30 @@
 package restaurant;
 
 public class MenuItem {
+    private String description;
+    private String category;
+    private double price = 0.0;
+
+    public void setPrice(double newPrice){
+        price=newPrice;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    public void setDescription(String newDescription){
+        description=newDescription;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public void setCategory(String newCategory){
+        category=newCategory;
+    }
+    public String getCategory(){
+        return category;
+    }
+
+
 }
